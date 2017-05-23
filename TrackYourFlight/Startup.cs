508288559c51cd6c19b8c8a277741fe132969e53
DmitriyBalykin <dmitriy.balykin@gmail.com>
2017-05-23 16:45:20 +0300

    Initial commit

---
+++ TrackYourFlight/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TrackYourFlight.Startup))]
+namespace TrackYourFlight
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

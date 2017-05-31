b3988dade9f49c6e49d201ae4bfd02d3f7f5a569
DmitriyBalykin <dmitriy.balykin@gmail.com>
2017-05-31 19:07:16 +0300

    Remove excessive files

--- TrackYourFlight/Controllers/MeteoController.cs
+++ TrackYourFlight/Controllers/MeteoController.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Mvc;
 
 namespace TrackYourFlight.Controllers
 {
@@ -8,6 +11,34 @@ namespace TrackYourFlight.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        private const string BaseUrl = "https://rucsoundings.noaa.gov/get_soundings.cgi?data_source=GFS&latest=latest";
+
+        public async Task<ActionResult> Data()
+        {
+            var httpClient = new HttpClient();
+            var uri = GetGfsDataUrl(2017, "May", 26, 10, 0, 36, 50.5, 30.5);
+
+            var result = await httpClient.GetStringAsync(uri);
+
+            return new JsonResult();
+        }
+
+        private static Uri GetGfsDataUrl(int year, string monthName, int day, int hour, int minute, int hoursInterval, double latitude, double longitude)
+        {
+            var url = BaseUrl +
+                "&start_year=" + year +
+                "&start_month_name=" + monthName +
+                "&start_mday=" + day +
+                "&start_hour=" + hour +
+                "&start_min=" + minute +
+                "&n_hrs=" + hoursInterval +
+                "&fcst_len=shortest" +
+                "&airport=" + latitude + "%2C" + longitude +
+                "&text=Ascii%20text%20%28GSD%20format%29&hydrometeors=false&start=latest";
+
+            return new Uri(url);
         }
     }
 }
--- TrackYourFlight/Scripts/site/meteo.js
+++ TrackYourFlight/Scripts/site/meteo.js
@@ -1,28 +1,10 @@
-﻿var baseUrl = 'https://rucsoundings.noaa.gov/get_soundings.cgi?data_source=GFS&latest=latest';
-
-var gfsUrlBuilder = function (year, monthName, day, hour, minute, hoursInterval, latitude, longitude) {
-    var url = baseUrl +
-        '&start_year=' + year +
-        '&start_month_name=' + monthName +
-        '&start_mday=' + day +
-        '&start_hour=' + hour +
-        '&start_min=' + minute +
-        '&n_hrs=' + hoursInterval +
-        '&fcst_len=shortest' +
-        '&airport=' + latitude + '%2C' + longitude +
-        '&text=Ascii%20text%20%28GSD%20format%29&hydrometeors=false&start=latest';
-
-    return url;
-};
-
+﻿
 $(document).ready(function () {
-
-    var url = gfsUrlBuilder(2017, 'May', 26, 10, 0, 36, 50.5, 30.5);
 
     $.ajax({
         type: 'GET',
-        dataType: 'jsonp',
-        url: url,
+        dataType: 'json',
+        url: 'Meteo/Data',
         success: function (response) {
             var meteElement = $('#meteoTable');
 
